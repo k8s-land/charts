bd68539b9aa40005cd65027b82874d85eb462751
Charlie Drage <charlie@charliedrage.com>
2019-12-12 13:57:03 -0500

    Update, add gitea + modify script
    Signed-off-by: Charlie Drage <charlie@charliedrage.com>

--- update.sh
+++ update.sh
@@ -2,27 +2,26 @@
 
 set -e
 
-git checkout gh-pages
+# List of charts
+charts=("gitea")
 
-CURRENT_DIR=$PWD
-DIR=`mktemp -d`
-
-cd $DIR
-
-git clone https://github.com/cdrage/charts && cd charts
-
-for i in */; do
+# Lets generate the packages!
+mkdir tmp
+cd tmp
+for i in "${charts[@]}"; do
+  git clone https://github.com/k8s-land/$i-chart $i
   helm package -f $i/values.yaml --sign --key "Charlie Drage <charlie@charliedrage.com>" --keyring ~/.gnupg/secring.gpg $i
 done
 
-mv *.tgz *.tgz.prov $CURRENT_DIR
-
-cd $CURRENT_DIR
+# Move the files
+cd ..
+mv tmp/*.tgz* .
+rm -rf tmp
 
 helm repo index .
 
-git add .
-DATE=`date`
-git commit -m "Update $DATE"
-git push origin gh-pages
-git checkout master
+#git add .
+#DATE=`date`
+#git commit -m "Update $DATE"
+#git push origin gh-pages
+#git checkout master
